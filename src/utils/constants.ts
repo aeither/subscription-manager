31cf582b0bf30ee06a23653abd591ee89fd4af87
aeither <36173828+aeither@users.noreply.github.com>
2023-08-21 18:07:14 +0200

    .

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
-export const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
+// export const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
+export const EASContractAddress = "0xAcfE09Fd03f7812F022FBf636700AdEA18Fd2A7A"; // Goerli Testner
 export const schemaUID = "0x83422a9c21920ff23e2ca62c3fbd52536c7d90798a6ea0e6926dcc82afc06050";
-export const RPC_URL = "https://sepolia.gateway.tenderly.co/";
-// export const RPC_URL = "https://base-goerli.gateway.tenderly.co/";
+// export const RPC_URL = "https://sepolia.gateway.tenderly.co/";
+export const RPC_URL = "https://base-goerli.gateway.tenderly.co/";
