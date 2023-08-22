499403ae0edaedded0abe4cfa0981153ee075d6f
aeither <36173828+aeither@users.noreply.github.com>
2023-08-22 09:39:35 +0200

    int graphql

--- src/list.tsx
+++ src/list.tsx
@@ -1,58 +1,108 @@
-import { EAS } from "@ethereum-attestation-service/eas-sdk";
+import { EAS, SchemaRegistry } from "@ethereum-attestation-service/eas-sdk";
 import { SignerOrProvider } from "@ethereum-attestation-service/eas-sdk/dist/transaction";
-import { Action, ActionPanel, Form, showToast } from "@raycast/api";
+import { List } from "@raycast/api";
+import { useCachedPromise, usePromise } from "@raycast/utils";
+import { EASContractAddress, schemaUID } from "./utils/constants";
+import { getSigner } from "./utils/signer";
 import { ethers } from "ethers";
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, createHttpLink } from "@apollo/client";
+import fetch from "cross-fetch";
 
-type Values = {
-  textfield: string;
-  textarea: string;
-  datepicker: Date;
-  checkbox: boolean;
-  dropdown: string;
-  tokeneditor: string[];
+const httpLink = createHttpLink({
+  uri: "https://base-goerli.easscan.org/graphql",
+  fetch,
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+const GET_SCHEMA = gql`
+  query ExampleQuery($where: SchemaWhereUniqueInput!) {
+    getSchema(where: $where) {
+      attestations {
+        recipient
+        attester
+        expirationTime
+        revoked
+        decodedDataJson
+        txid
+      }
+    }
+  }
+`;
+
+const getList = async () => {
+  const result = await client.query({
+    query: GET_SCHEMA,
+    variables: {
+      where: {
+        id: "0x83422a9c21920ff23e2ca62c3fbd52536c7d90798a6ea0e6926dcc82afc06050",
+      },
+    },
+  });
+
+  console.log("🚀 ~ file: list.tsx:66 ~ getClient ~ result:", result);
+  return result;
 };
 
 export default function Command() {
-  async function handleSubmit(values: Values) {
-    const uid = "0xff08bbf3d3e6e0992fc70ab9b9370416be59e87897c3d42b20549901d2cccc3e";
+  // const { isLoading, data, revalidate } = usePromise(async () => {
+  //   const eas = new EAS(EASContractAddress);
+  //   const signer = await getSigner();
+  //   const provider = ethers.providers.getDefaultProvider("sepolia");
+  //   eas.connect(signer as unknown as SignerOrProvider);
 
-    // Initialize the sdk with the address of the EAS Schema contract address
-    const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
-    const eas = new EAS(EASContractAddress);
+  //   const schemaRegistryContractAddress = "0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0"; // Sepolia 0.26
+  //   const schemaRegistry = new SchemaRegistry(schemaRegistryContractAddress);
+  //   schemaRegistry.connect(provider as unknown as SignerOrProvider);
 
-    const provider = new ethers.providers.JsonRpcProvider(`https://base-goerli.gateway.tenderly.co/`)
+  //   const schemaRecord = await schemaRegistry.getSchema({ uid: schemaUID });
 
-    // Connects an ethers style provider/signingProvider to perform read/write functions.
-    // MUST be a signer to do write operations!
-    eas.connect(provider as unknown as SignerOrProvider);
-    const attestation = await eas.getAttestation(uid);
+  //   console.log(schemaRecord);
+  //   // return attestation;
+  // }, []);
 
-    console.log(attestation);
+  const { isLoading, error, data } = useCachedPromise(getList, [], {
+    keepPreviousData: true,
+  });
 
-    console.log(values);
-    showToast({ title: "Submitted form", message: "See logs for submitted values" });
-  }
+  // Use the data here as needed
+  console.log(data);
 
   return (
-    <Form
-      actions={
-        <ActionPanel>
-          <Action.SubmitForm onSubmit={handleSubmit} />
-        </ActionPanel>
-      }
-    >
-      <Form.Description text="This form showcases all available form elements." />
-      <Form.TextField id="textfield" title="Text field" placeholder="Enter text" defaultValue="Raycast" />
-      <Form.TextArea id="textarea" title="Text area" placeholder="Enter multi-line text" />
-      <Form.Separator />
-      <Form.DatePicker id="datepicker" title="Date picker" />
-      <Form.Checkbox id="checkbox" title="Checkbox" label="Checkbox Label" storeValue />
-      <Form.Dropdown id="dropdown" title="Dropdown">
-        <Form.Dropdown.Item value="dropdown-item" title="Dropdown Item" />
-      </Form.Dropdown>
-      <Form.TagPicker id="tokeneditor" title="Tag picker">
-        <Form.TagPicker.Item value="tagpicker-item" title="Tag Picker Item" />
-      </Form.TagPicker>
-    </Form>
+    <ApolloProvider client={client}>
+      <List isLoading={isLoading}>
+        <List.EmptyView title={"loading..."} />
+        
+        <List.Item title="Item 1" />
+        <List.Item title="Item 2" subtitle="Optional subtitle" />
+      </List>
+    </ApolloProvider>
+  );
+}
+
+function App() {
+  // Use the useQuery hook to fetch data
+  const { loading, error, data } = useQuery(GET_SCHEMA, {
+    variables: {
+      where: {
+        id: "0x83422a9c21920ff23e2ca62c3fbd52536c7d90798a6ea0e6926dcc82afc06050",
+      },
+    },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  // Use the data here as needed
+  console.log(data);
+
+  return (
+    <List>
+      <List.Item title="Item 1" />
+      <List.Item title="Item 2" subtitle="Optional subtitle" />
+    </List>
   );
 }
