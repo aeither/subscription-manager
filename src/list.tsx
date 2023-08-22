cd5fdef9e03623758d14ff3f083d1367f938415b
aeither <36173828+aeither@users.noreply.github.com>
2023-08-22 09:51:22 +0200

    show attestations

--- src/list.tsx
+++ src/list.tsx
@@ -1,12 +1,22 @@
-import { EAS, SchemaRegistry } from "@ethereum-attestation-service/eas-sdk";
-import { SignerOrProvider } from "@ethereum-attestation-service/eas-sdk/dist/transaction";
-import { List } from "@raycast/api";
-import { useCachedPromise, usePromise } from "@raycast/utils";
-import { EASContractAddress, schemaUID } from "./utils/constants";
-import { getSigner } from "./utils/signer";
-import { ethers } from "ethers";
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, createHttpLink } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, gql } from "@apollo/client";
+import { Action, ActionPanel, Detail, List } from "@raycast/api";
+import { useCachedPromise } from "@raycast/utils";
 import fetch from "cross-fetch";
+import { schemaUID } from "./utils/constants";
+
+interface AttestationData {
+  getSchema: {
+    attestations: Attestation[];
+  };
+}
+interface Attestation {
+  recipient: string;
+  attester: string;
+  expirationTime: string; // You might want to use a Date type if appropriate
+  revoked: boolean;
+  decodedDataJson: string; // You might want to use a more specific type based on the actual data structure
+  txid: string;
+}
 
 const httpLink = createHttpLink({
   uri: "https://base-goerli.easscan.org/graphql",
@@ -34,36 +44,19 @@ const GET_SCHEMA = gql`
 `;
 
 const getList = async () => {
-  const result = await client.query({
+  const result = await client.query<AttestationData>({
     query: GET_SCHEMA,
     variables: {
       where: {
-        id: "0x83422a9c21920ff23e2ca62c3fbd52536c7d90798a6ea0e6926dcc82afc06050",
+        id: schemaUID,
       },
     },
   });
 
-  console.log("🚀 ~ file: list.tsx:66 ~ getClient ~ result:", result);
-  return result;
+  return result.data;
 };
 
 export default function Command() {
-  // const { isLoading, data, revalidate } = usePromise(async () => {
-  //   const eas = new EAS(EASContractAddress);
-  //   const signer = await getSigner();
-  //   const provider = ethers.providers.getDefaultProvider("sepolia");
-  //   eas.connect(signer as unknown as SignerOrProvider);
-
-  //   const schemaRegistryContractAddress = "0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0"; // Sepolia 0.26
-  //   const schemaRegistry = new SchemaRegistry(schemaRegistryContractAddress);
-  //   schemaRegistry.connect(provider as unknown as SignerOrProvider);
-
-  //   const schemaRecord = await schemaRegistry.getSchema({ uid: schemaUID });
-
-  //   console.log(schemaRecord);
-  //   // return attestation;
-  // }, []);
-
   const { isLoading, error, data } = useCachedPromise(getList, [], {
     keepPreviousData: true,
   });
@@ -75,34 +68,23 @@ export default function Command() {
     <ApolloProvider client={client}>
       <List isLoading={isLoading}>
         <List.EmptyView title={"loading..."} />
-        
-        <List.Item title="Item 1" />
-        <List.Item title="Item 2" subtitle="Optional subtitle" />
+
+        {data?.getSchema.attestations.map((atts) => (
+          <List.Item
+            key={atts.txid}
+            title={atts.txid}
+            actions={
+              <ActionPanel>
+                <Action.Push title="Show Details" target={<DetailPage />} />
+              </ActionPanel>
+            }
+          />
+        ))}
       </List>
     </ApolloProvider>
   );
 }
 
-function App() {
-  // Use the useQuery hook to fetch data
-  const { loading, error, data } = useQuery(GET_SCHEMA, {
-    variables: {
-      where: {
-        id: "0x83422a9c21920ff23e2ca62c3fbd52536c7d90798a6ea0e6926dcc82afc06050",
-      },
-    },
-  });
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
-  // Use the data here as needed
-  console.log(data);
-
-  return (
-    <List>
-      <List.Item title="Item 1" />
-      <List.Item title="Item 2" subtitle="Optional subtitle" />
-    </List>
-  );
+function DetailPage() {
+  return <Detail markdown="DetailPage" />;
 }
