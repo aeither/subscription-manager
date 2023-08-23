c301b7e223899dd6af0993b6f88f631b489d8d13
aeither <36173828+aeither@users.noreply.github.com>
2023-08-23 12:45:05 +0200

    subscription simulate

--- src/subscribe.tsx
+++ src/subscribe.tsx
@@ -1,46 +1,81 @@
-import { Action, ActionPanel, Detail, getPreferenceValues } from "@raycast/api";
+import { Action, ActionPanel, Detail, Form, Grid, getPreferenceValues } from "@raycast/api";
 import "cross-fetch/polyfill";
-import { formatEther, http } from "viem";
+import { formatEther, http, parseEther } from "viem";
 import { baseGoerli } from "viem/chains";
 
 import { createWalletClient, publicActions } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
+import { SubscriptionAddress } from "./utils/constants";
+import { subscriptionAbi } from "./utils/subscriptionAbi";
 
 export default function Command() {
   const preferences = getPreferenceValues<Preferences>();
   const apiKey = preferences.apiKey;
   const account = privateKeyToAccount(apiKey as `0x${string}`);
 
-  const getInfo = async () => {
+  const handleSubmit = async () => {
     const client = createWalletClient({
       account,
       chain: baseGoerli,
       transport: http(),
     }).extend(publicActions);
 
-    // const { request } = await client.simulateContract({
-    //   address: "0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2",
-    //   abi: parseAbi(["function mint(uint32) view returns (uint32)"]),
-    //   functionName: "mint",
-    //   args: [69420],
-    //   account: "0xA0Cf798816D4b9b9866b5330EEa46a18382f251e",
-    // });
-    // const hash = await client.writeContract(request); // Wallet Action
+    const { result, request } = await client.simulateContract({
+      address: SubscriptionAddress,
+      abi: subscriptionAbi,
+      functionName: "subscribe",
+      value: parseEther("0.0001"),
+    });
+    console.log("🚀 ~ file: subscribe.tsx:37 ~ getInfo ~ result:", result, request);
 
-    const accountBalance = await client.getBalance({ address: account.address });
-    const balanceAsEther = formatEther(accountBalance);
+    // const accountBalance = await client.getBalance({ address: account.address });
+    // const balanceAsEther = formatEther(accountBalance);
 
-    console.log("🚀 ~ file: subscribe.tsx:29 ~ getInfo ~ account.address:", account.address);
-    console.log("🚀 ~ file: subscribe.tsx:13 ~ getInfo ~ accountBalance:", balanceAsEther);
+    // console.log("🚀 ~ file: subscribe.tsx:29 ~ getInfo ~ account.address:", account.address);
+    // console.log("🚀 ~ file: subscribe.tsx:13 ~ getInfo ~ accountBalance:", balanceAsEther);
   };
+
+  // 2,592,000,000
   return (
-    <Detail
-      markdown={"hello world"}
-      actions={
-        <ActionPanel>
-          <Action title="Open Extension Preferences" onAction={getInfo} />
-        </ActionPanel>
-      }
-    />
+    <Grid columns={3}>
+      <Grid.Item
+        content="⭐️"
+        title="Starter Plan"
+        actions={
+          <ActionPanel>
+            <Action.SubmitForm onSubmit={handleSubmit} />
+          </ActionPanel>
+        }
+      />
+      <Grid.Item
+        content="🌟"
+        title="Premium Plan"
+        actions={
+          <ActionPanel>
+            <Action.CopyToClipboard content="👋" />
+          </ActionPanel>
+        }
+      />
+      <Grid.Item
+        content="🤩"
+        title="Ultra Plan"
+        actions={
+          <ActionPanel>
+            <Action.CopyToClipboard content="👋" />
+          </ActionPanel>
+        }
+      />
+    </Grid>
+
+    // <Form
+    //   actions={
+    //     <ActionPanel>
+    //       <Action.SubmitForm onSubmit={handleSubmit} />
+    //     </ActionPanel>
+    //   }
+    // >
+    //   <Form.Description text="Subscribe for 30 days to basic Plan" />
+    //   <Form.TextField id="price" title="Price in ETH" defaultValue="20" />
+    // </Form>
   );
 }
---
+++ src/utils/subscriptionAbi.ts
@@ -0,0 +1,98 @@
+export const subscriptionAbi = [
+  {
+    inputs: [],
+    name: "subscribe",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "subscriber",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "Subscription",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "withdrawFunds",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getContractBalance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getNumberOfSubscribers",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "subscriptions",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const;
